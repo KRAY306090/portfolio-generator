f281520e2ed9a0f308ecded5d8cf89ddb23ae0f7
Kennadee <kennadee.ray@gmail.com>
2020-07-30 13:25:40 -0500

    generates webpage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
+/* const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
     }
@@ -8,4 +11,9 @@ const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+      if(err) throw err;
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  });
