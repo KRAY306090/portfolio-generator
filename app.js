b30a4e269ed496a23e3ff7793302e6f5a6b2c7ee
Kennadee <kennadee.ray@gmail.com>
2020-07-30 13:49:44 -0500

    Inquirer installed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const [name, github] = profileDataArgs;
 
 /* const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -13,7 +13,17 @@ const [name, github] = profileDataArgs;
 
 printProfileData(profileDataArgs); */
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-      if(err) throw err;
-      console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+ // fs.writeFile('index.html', generatePage(name, github), err => {
+      //if(err) throw err;
+     // console.log('Portfolio complete! Check out index.html to see the output!');
+//  });
+const inquirer = require("inquirer");
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
